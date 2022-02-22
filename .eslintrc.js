eb88ce82f2289a04b5050cd12ae403e882f8d7bd
MonzonFrancisco <gallefrancisco1997@gmail.com>
2022-02-22 00:17:32 -0500

    Se sube el proyecto de la calculadora

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'prettier/prettier': 0,
+        'react-native/no-inline-styles': 0,
+      },
+    },
+  ],
+};
